76b7487959af7349f4c0fdc99134ac130e2e7694
Kasper4113 <Kaspervalstedpetersen@gmail.com>
2026-05-12 13:41:16 +0200

    Kontakt side, og footer
    Sat footer ind, og opsat forsiden korrekt, samt fået validering til at virke

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,3 +33,23 @@ tiles.forEach(tile => {
 		setTimeout(() => tile.classList.remove('selected'), 300);
 	});
 });
+
+
+const nameInput = document.getElementById("nameinputer");
+const ageInput = document.getElementById("ageinputer");
+const mailInput = document.getElementById("mailinputer");
+const button = document.getElementById("buttoner");
+ 
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    let nameValue = nameInput.value;
+    let ageValue = ageInput.value;
+    let mailValue = mailInput.value;
+    console.log(nameValue, ageValue, mailValue);
+if (nameValue.length >= 3 && ageValue > 18 && mailValue == "kaspervalstedpetersen@gmail.com") {
+    alert("Velkommen!");
+}
+else {
+    alert ("Du opflyder ikke alle kravene, prøv igen!");
+}
+});
